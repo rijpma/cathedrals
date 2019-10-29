60d7c028d6c4310e2c3222d0904d2d0589962364
rijpma <auke.rijpma@gmail.com>
2019-10-29 23:50:56 +0100

    update names

--- reproduce.sh
+++ reproduce.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# data creation
-Rscript --verbose script/cat_siem.R > siem.Rout 2>&1
-Rscript --verbose script/cat_auxdata.R > auxdata.Rout 2>&1
-Rscript --verbose script/cat_dataprep.R > dataprep.Rout 2>&1 # take a few minutes
+# data prep
+Rscript --verbose script/prep_citydata.R > prep_citydata.Rout 2>&1
+Rscript --verbose script/prep_popdata.R > prep_popdata.Rout 2>&1
+Rscript --verbose script/prep_churchdata.R > prep_churchdata.Rout 2>&1 # take a few minutes
+Rscript --verbose script/prep_disasterdata.R > prep_disasterdata.Rout 2>&1
 
 # main paper tables and figurs
-Rscript --verbose script/cat_sumstats.R > sumstats.Rout 2>&1
-Rscript --verbose script/cat_explorations.R > explorations.Rout 2>&1
-Rscript --verbose script/cat_stockact.R > stockact.Rout 2>&1
-Rscript --verbose script/cat_rasters.R > rasters.Rout 2>&1
-Rscript --verbose script/cat_samplesize.R > samplesize.Rout 2>&1
+Rscript --verbose script/sumstats.R > sumstats.Rout 2>&1
+Rscript --verbose script/explorations.R > explorations.Rout 2>&1
+Rscript --verbose script/stockact.R > stockact.Rout 2>&1
+Rscript --verbose script/rasters.R > rasters.Rout 2>&1
+Rscript --verbose script/samplesize.R > samplesize.Rout 2>&1
 
 # appendices tables and figures
-Rscript --verbose script/cat_heightimps.R > heightimps.Rout 2>&1
-Rscript --verbose script/cat_m2imps.R > m2imps.Rout 2>&1 # takes a few minutes
-Rscript --verbose script/cat_james.R > james.Rout 2>&1
-Rscript --verbose script/cat_ruralsample.R > ruralsample.Rout 2>&1
-Rscript --verbose script/cat_disasters.R > disasters.Rout 2>&1
-Rscript --verbose script/cat_disastercorrect.R > disastercorrect.Rout 2>&1
+Rscript --verbose script/heightimps.R > heightimps.Rout 2>&1
+Rscript --verbose script/m2imps.R > m2imps.Rout 2>&1 # takes a few minutes
+Rscript --verbose script/jamescompare.R > jamescompare.Rout 2>&1
+Rscript --verbose script/ruralsample.R > ruralsample.Rout 2>&1
+Rscript --verbose script/disastercorrect.R > disastercorrect.Rout 2>&1
